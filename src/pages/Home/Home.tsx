eea5f7c2409b08b64662f914b529c4d1dc8bce6f
user_nitro_5 <timmy.lepel@gmail.com>
2021-11-24 23:17:37 +0300

    description home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,10 @@ import React, {FC} from 'react'
 export const Home: FC = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1> LEARNING CARDS</h1>
+            <h2> Here you can create your own card packs with questions and answers.
+                Then you can check your skills trying to give give answers to cards and grading your success or failure.
+                Future cards will be offered based on personal grade.</h2>
         </div>
     )
 }
